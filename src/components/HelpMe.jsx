c8f5e311aa442b5734333f0eb1661eaa031734c8
atomicjed <jed.ryan@atomicmedia.co.uk>
2024-11-26 17:35:15 +0000

    Module 1 Video

--- src/components/HelpMe.jsx
+++ src/components/HelpMe.jsx
@@ -4,6 +4,6 @@ export default function HelpMe() {
   const navigate = useNavigate();
   
   return (
-    <div onClick={() => navigate('/help-me')} className={'sm:-rotate-90 fixed bottom-0 sm:bottom-1/2 left-0 sm:-left-12 z-10 bg-primary text-white px-8 py-4 sm:pt-8 sm:pb-4 rounded-tr-2xl sm:rounded-b-2xl transition duration-300 ease-in-out sm:hover:translate-x-1.5 cursor-pointer font-medium sm:font-black'}>HELP ME!</div>
+    <div onClick={() => navigate('/help-me')} className={'md:-rotate-90 fixed bottom-0 md:bottom-1/2 left-0 md:-left-12 z-10 bg-primary text-white px-8 py-4 md:pt-8 md:pb-4 rounded-tr-2xl md:rounded-b-2xl transition duration-300 ease-in-out md:hover:translate-x-1.5 cursor-pointer font-medium md:font-black'}>HELP ME!</div>
   )
 }
--- src/modules/ModuleOne.jsx
+++ src/modules/ModuleOne.jsx
@@ -39,8 +39,12 @@ export default function ModuleOne({ moduleNumber }) {
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header navigation={module1NavItems}/>
-        <Hero title={"Python Basics"}
-              subHeading={"\"Fear not the man who has practiced 10,000 kicks once, fear the man who has practiced one kick 10,000 times.\""}/>
+        <Hero 
+          title={"Python Basics"}
+          subHeading={"\"Fear not the man who has practiced 10,000 kicks once, fear the man who has practiced one kick 10,000 times.\""}
+          video={'https://firebasestorage.googleapis.com/v0/b/python-cv-course-a91b0.firebasestorage.app/o/CourseIntro.mp4?alt=media&token=fae79143-daf3-4493-b72a-95846396103d'}
+          videoThumbnail={'https://firebasestorage.googleapis.com/v0/b/python-cv-course-a91b0.firebasestorage.app/o/CourseIntroThumbnail.png?alt=media&token=6f74ebef-16de-4f38-b86a-18406df16b72'}
+        />
         <InfoSection sectionId={'introduction'} content={whatIsPythonContent} isFirst/>
         <InfoSection sectionId={'data-types'} content={dataTypesContent}/>
         <InfoSection sectionId={'data-structures'} content={dataStructuresContent}/>
